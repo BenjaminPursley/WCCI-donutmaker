64c5eba55838451e1d004042f557ee33fdf1e7ff
benjaminpursley <benpursley27@gmail.com>
2023-06-09 17:24:35 -0400

    added basic buttons and total print function

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ btn.addEventListener("click", function () {
 
 
 const button1 = document.getElementById("button1")
+const button2 = document.getElementById("button2")
+const button3 = document.getElementById("button3")
 const bigText = document.getElementById("page-header")
 const paraText = document.querySelector("p")
 //using the querySelectorALL function generates
@@ -15,12 +17,36 @@ const paraText = document.querySelector("p")
 const allOurToasters = document.querySelectorAll(".toaster");
 
 let donutTotal = 0
+let autoClickerTotal = 0
+let autoClickerCost = 100
+
+
+function updateTotalDisplay() {
+  bigText.innerText = "You now have " + donutTotal + " donuts!";
+  if (donutTotal < 200) {
+    let donutPixels = `${donutTotal}px`
+    paraText.style.fontSize = donutPixels
+  }
+}
 
 button1.addEventListener("click", function () {
     donutTotal++
-    bigText.innerText = "You now have " + donutTotal + " donuts!";
-    let donutPixels = `${donutTotal}px`
-    paraText.style.fontSize = donutPixels
+    updateTotalDisplay()
+})
+
+button2.addEventListener("click", function () {
+  if (donutTotal > autoClickerCost) {
+    donutTotal-=autoClickerCost
+    autoClickerTotal++
+    autoClickerCost+=10
+    updateTotalDisplay()
+  }
+})
+
+//Button for testing purposes
+button3.addEventListener("click", function () {
+  donutTotal+=100
+  updateTotalDisplay()
 })
 
 
