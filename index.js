535ce82706931633a74cd1bac0ecae2f3a7b3dde
benjaminpursley <benpursley27@gmail.com>
2023-06-09 20:48:28 -0400

    added auto-clicker info messages

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-//list of constants
+//list of variables
 const button1 = document.getElementById("button1")
 const button2 = document.getElementById("button2")
 const button3 = document.getElementById("button3")
-const bigText = document.getElementById("page-header")
+const donutDisplay = document.getElementById("donut-display")
+const autoClickerDisplay = document.getElementById("auto-clicker-display")
+const autoClickerPriceTag = document.getElementById("auto-clicker-price-tag")
 const paraText = document.querySelector("p")
-//using the querySelectorALL function generates
-//an array of all the searched for elements
-const allOurToasters = document.querySelectorAll(".toaster");
 
 let donutCount = 0
 let autoClickerCount = 0
@@ -21,7 +20,7 @@ function addDonutForClick() {
 }
 
 function displayDonutCount() {
-  bigText.innerText = "You now have " + donutCount.toFixed(0) + " donuts!";
+  donutDisplay.innerText = "You now have " + donutCount.toFixed(0) + " donuts!";
   if (donutCount < 200) {
     let donutPixels = `${donutCount}px`
     paraText.style.fontSize = donutPixels
@@ -33,6 +32,7 @@ function purchaseAutoClicker() {
     donutCount-=autoClickerCost
     increaseAutoClickerCount()
     increaseAutoClickerCost()
+    updateAutoClickerInfo()
   } else {
     alert("Sorry, you don't have enough donuts.")
   }
@@ -61,9 +61,14 @@ function activateAutoClicker() {
 
 //Resets the game state. This action should reset the game to zero donuts and zero Auto Clickers
 
+//additional functions to satisfy assignment requirements
+function updateAutoClickerInfo() {
+  autoClickerDisplay.innerText = "You have " + autoClickerCount.toFixed(0) + " auto-clickers.";
+  autoClickerPriceTag.innerText = "An auto-clicker currently costs " + autoClickerCost.toFixed(0) + " donuts.";
+}
 
 
-
+//button functionality
 button1.addEventListener("click", function () {
   addDonutForClick()
   displayDonutCount()
@@ -81,28 +86,3 @@ button3.addEventListener("click", function () {
 })
 
 
-allOurToasters.forEach((myToaster) => {
-    myToaster.style.color = "royalblue";
-    console.log(myToaster)
-})
-
-/*
-remove text
-bigText.remove()
-*/
-
-
-/*
-//create a variable to track increments
-let counter = 0
-
-
-function countUp() {
-    //this function increments the counter
-    counter+=77;
-    const bigText = document.getElementById("page-header");
-    bigText.innerText = counter
-}
-
-const myInterval = setInterval(countUp, 1000)
-*/
